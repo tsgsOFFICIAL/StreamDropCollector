1929920167855cd8afc2189a46b43b07aab5fe7f
Marcus Jensen <marcus.jensen@emerson.com>
2025-11-20 11:35:58 +0100

    Removed models

--- Models/AccountInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Models
-{
-    public class AccountInfo
-    {
-        public string Username { get; set; } = "";
-        public string Password { get; set; } = "";
-        public string Token { get; set; } = "";
-    }
-}
--- Models/StreamInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Models;
-
-public class StreamInfo
-{
-    public string StreamId { get; set; } = "";
-    public string Title { get; set; } = "";
-    public string Streamer { get; set; } = "";
-    public bool HasDrops { get; set; }
-}
