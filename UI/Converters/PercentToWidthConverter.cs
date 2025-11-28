0d135245111683ab602a02ee9bc3567c1fee56e5
tsgsOFFICIAL <marcusproman0507@gmail.com>
2025-11-28 21:02:01 +0100

    Finally fixed the progress bars

---
+++ UI/Converters/PercentToWidthConverter.cs
@@ -0,0 +1,16 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace UI.Converters
+{
+    public class PercentToWidthConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type t, object p, CultureInfo c)
+        {
+            if (values.Length == 2 && values[0] is double percent && values[1] is double width)
+                return percent / 100.0 * width;
+            return 0.0;
+        }
+        public object[] ConvertBack(object v, Type[] t, object p, CultureInfo c) => throw new NotSupportedException();
+    }
+}
