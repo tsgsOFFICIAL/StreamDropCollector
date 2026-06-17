c00287b3b0518f8cec63b02056c82b3ae3946a90
tsgsOFFICIAL <marcusproman0507@gmail.com>
2026-06-17 12:27:51 +0200

    Added PlatformConnectionState for UserControl

---
+++ UI/Models/PlatformConnectionState.cs
@@ -0,0 +1,51 @@
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace UI.Models
+{
+    public sealed class PlatformConnectionState : INotifyPropertyChanged
+    {
+        public PlatformConnectionState(string platformName, string brandBrushKey, string loginButtonLabel)
+        {
+            PlatformName = platformName;
+            BrandBrushKey = brandBrushKey;
+            _loginButtonText = loginButtonLabel;
+        }
+
+        public string PlatformName { get; }
+        public string BrandBrushKey { get; }
+
+        private string _connectionStatus = "Not Connected";
+        public string ConnectionStatus
+        {
+            get => _connectionStatus;
+            set { _connectionStatus = value; OnPropertyChanged(); }
+        }
+
+        private string _connectionColor = "Red";
+        public string ConnectionColor
+        {
+            get => _connectionColor;
+            set { _connectionColor = value; OnPropertyChanged(); }
+        }
+
+        private string _loginButtonText;
+        public string LoginButtonText
+        {
+            get => _loginButtonText;
+            set { _loginButtonText = value; OnPropertyChanged(); }
+        }
+
+        private bool _isLoginEnabled = true;
+        public bool IsLoginEnabled
+        {
+            get => _isLoginEnabled;
+            set { _isLoginEnabled = value; OnPropertyChanged(); }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private void OnPropertyChanged([CallerMemberName] string? name = null) =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+    }
+}
