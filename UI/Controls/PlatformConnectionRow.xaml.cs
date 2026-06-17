d8e4709613a1e814ccab1c5db72b1abe5091ff8f
tsgsOFFICIAL <marcusproman0507@gmail.com>
2026-06-17 12:26:53 +0200

    Added PlatformConnectionRow for better readability / Code management

---
+++ UI/Controls/PlatformConnectionRow.xaml.cs
@@ -0,0 +1,12 @@
+using UserControl = System.Windows.Controls.UserControl;
+
+namespace UI.Controls
+{
+    public partial class PlatformConnectionRow : UserControl
+    {
+        public PlatformConnectionRow()
+        {
+            InitializeComponent();
+        }
+    }
+}
